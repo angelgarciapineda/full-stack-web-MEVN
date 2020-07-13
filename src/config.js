7315ac974106efb295ed207a82840ba95ffa6d73
Luis Angel GARCIA <garciapinedaluisangel@gmail.com>
2020-07-13 15:00:04 +0200

    Conexión a base de datos en local

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    port: process.env.PORT || 3000,
-    db: process.env.MONGODB || "mongodb+srv://angelgarcia:e19d036j@cluster0-9ha18.mongodb.net/projetPanneaux?retryWrites=true&w=majority",
-    SECRET_TOKEN: "miclavedetokens"
-  };
+  port: process.env.PORT || 3000,
+  db: process.env.MONGODB || "mongodb://localhost:27017/projetPanneaux",
+  /*db: process.env.MONGODB || "mongodb+srv://angelgarcia:e19d036j@cluster0-9ha18.mongodb.net/projetPanneaux?retryWrites=true&w=majority",*/
+  SECRET_TOKEN: "miclavedetokens"
+};
